9499970d5399fa5766601ad91a1588698fce4c36
Neema <neema.edward@student.moringaschool.com>
2025-04-29 11:42:08 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
-import React from "react";
+// src/components/App.js
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
-console.log(blogData);
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <Header name={blogData.name}/>
-      <About image={blogData.image} about={blogData.about}/>
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
-
-export default App;
